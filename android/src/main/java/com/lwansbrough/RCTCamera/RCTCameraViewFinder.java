bd1e7db74cd9810b562f3b0c479ac82060808b71
HFmoney <mk@hfmoney.com>
2017-10-16 20:34:02 -0500

    Update RCTCameraViewFinder.java
    修复扫码崩溃

--- android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraViewFinder.java
+++ android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraViewFinder.java
@@ -306,25 +306,25 @@ class RCTCameraViewFinder extends TextureView implements TextureView.SurfaceText
                 return null;
             }
 
-            Camera.Size size = camera.getParameters().getPreviewSize();
-
-            int width = size.width;
-            int height = size.height;
-
-            // rotate for zxing if orientation is portrait
-            if (RCTCamera.getInstance().getActualDeviceOrientation() == 0) {
-                byte[] rotated = new byte[imageData.length];
-                for (int y = 0; y < height; y++) {
-                    for (int x = 0; x < width; x++) {
-                        rotated[x * height + height - y - 1] = imageData[x + y * width];
-                    }
-                }
-                width = size.height;
-                height = size.width;
-                imageData = rotated;
-            }
-
             try {
+                Camera.Size size = camera.getParameters().getPreviewSize();
+
+                int width = size.width;
+                int height = size.height;
+
+                // rotate for zxing if orientation is portrait
+                if (RCTCamera.getInstance().getActualDeviceOrientation() == 0) {
+                    byte[] rotated = new byte[imageData.length];
+                    for (int y = 0; y < height; y++) {
+                        for (int x = 0; x < width; x++) {
+                            rotated[x * height + height - y - 1] = imageData[x + y * width];
+                        }
+                    }
+                    width = size.height;
+                    height = size.width;
+                    imageData = rotated;
+                }
+                
                 PlanarYUVLuminanceSource source = new PlanarYUVLuminanceSource(imageData, width, height, 0, 0, width, height, false);
                 BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
                 Result result = _multiFormatReader.decodeWithState(bitmap);
